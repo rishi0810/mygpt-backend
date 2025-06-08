c2556aa988de9275fa77b3f4efbe3e0ee292b742
rishi0810 <rishi.1003raj@gmail.com>
2025-06-08 21:52:25 +0530

    Prod Stuff

--- index.js
+++ index.js
@@ -7,19 +7,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send({message : "Get request initialized"});
-})
+  res.send({ message: "Get request initialized" });
+});
 
-app.post("/api/chat", async(req, res) => {
+app.post("/api/chat", async (req, res) => {
   const { prompt } = req.body;
   const response = await query(prompt);
-  res.send({response : response})
-})
+  res.send({ response: response });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-}) 
+});
